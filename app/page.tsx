d204f2c72e590f5b04e7e71fb9a479339a6afc85
ruru <142723369+ruru-m07@users.noreply.github.com>
2024-01-26 11:37:33 +0530

    👍 FIX build

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,11 @@
 
 import { ModeToggle } from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
-import { hello } from "@/server/actions/hello";
 
 export default function Home() {
-  const hendelclick = () => {
-    hello();
-  };
   return (
     <main>
       <ModeToggle />
-      <Button onClick={hendelclick}>hello</Button>
     </main>
   );
 }
