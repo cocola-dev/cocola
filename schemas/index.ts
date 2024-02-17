ba07b28932dc4f49cd0cea54dde65f444ef8f255
ruru <142723369+ruru-m07@users.noreply.github.com>
2024-02-17 21:05:37 +0530

    FIX: a little bug
    Co-Authored-By: ruru <ruru.dev07@gmail.com>

--- schemas/index.ts
+++ schemas/index.ts
@@ -7,7 +7,7 @@ export const SettingsSchema = z
   .object({
     name: z.optional(z.string()),
     isTwoFactorEnabled: z.optional(z.boolean()),
-    role: z.enum([UserRole.ADMIN, UserRole.USER]),
+    role: z.enum([UserRole.ADMIN, UserRole.USER, UserRole.BOT]),
     email: z.optional(z.string().email()),
     password: z.optional(z.string().min(6)),
     newPassword: z.optional(z.string().min(6)),
