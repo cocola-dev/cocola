60b9b5bd28a9d97f824d28d393607002cb53891b
ruru <142723369+ruru-m07@users.noreply.github.com>
2024-02-24 19:26:54 +0530

    setting page UI/UX Fixes #53

---
+++ app/settings/account/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div className="ml-8 text-2xl">Change username</div>;
+};
+
+export default Page;
---
+++ app/settings/notifications/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div className="ml-8 text-2xl">Theme preferences</div>;
+};
+
+export default Page;
