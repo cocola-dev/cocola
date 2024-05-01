867381595cbbd75bcb75524acdc2d5f34e50fa32
ruru <142723369+ruru-m07@users.noreply.github.com>
2024-05-01 12:52:22 +0530

    bug: fix broken link in email confirm

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,8 +1,7 @@
+import { PUBLIC_URL } from "@/resources";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-
-const domain = process.env.NEXT_PUBLIC_VERCEL_URL;
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
   const template = `
@@ -99,7 +98,7 @@ export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
 };
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-  const resetLink = `${domain}/new-password?token=${token}`;
+  const resetLink = `${PUBLIC_URL}/new-password?token=${token}`;
 
   await resend.emails.send({
     from: "noreply@cocola.tech",
@@ -110,7 +109,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
 };
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `${domain}/new-verification?token=${token}`;
+  const confirmLink = `${PUBLIC_URL}/new-verification?token=${token}`;
 
   await resend.emails.send({
     from: "noreply@cocola.tech",
